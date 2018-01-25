2df427ced0e2228a1203d4ba2e35a769bc6b9016
lsjhome <lsjhome007@gmail.com>
2018-01-25 15:56:24 +0900

    commit

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import re
+from bs4 import BeautifulSoup
+from utils import get_top100_list
 
-source = '''<td><div class="wrap">
-<div class="wrap_song_info">
-    <div class="ellipsis rank01"><span>
-        <a href="javascript:melon.play.playSong('19030101',30851703);" title="다른사람을 사랑하고 있어 재생">다른사람을 사랑하고 있어</a>
-    </span></div><br>
-    <div class="ellipsis rank02">
-        <a href="javascript:melon.link.goArtistDetail('514741');" title="수지 (SUZY) - 페이지 이동">수지 (SUZY)</a><span class="checkEllipsis" style="display: none;"><a href="javascript:melon.link.goArtistDetail('514741');" title="수지 (SUZY) - 페이지 이동">수지 (SUZY)</a></span>
-    </div>
-</div>
-</div></td>'''
+# source = open('melon.html', 'rt').read()
+# soup = BeautifulSoup(source, 'lxml')
 
-# 위의 소스에서 <div class="ellipsis rank01> ~ </div>부분의 텍스트를
-# div_rank01 변수에 할당
-pattern_div_rank01 = re.compile(r'<div class="ellipsis rank01">.*?</div>', re.DOTALL)
-div_rank01 = re.search(pattern_div_rank01, source).group()
-print(div_rank01)
+# result = []
+# for tr in soup.find_all('tr', class_='lst50'):
+#     rank = tr.find('span', class_='rank').text
+#     title = tr.find('div', class_='rank01').find('a').text
+#     artist = tr.find('div', class_='rank02').find('a').text
+#     album = tr.find('div', class_='rank03').find('a').text
+#     url_img_cover = tr.find('a', class_='image_typeAll').find('img').get('src')
+#     # http://cdnimg.melon.co.kr/cm/album/images/101/28/855/10128855_500.jpg/melon/resize/120/quality/80/optimize
+#     # .* -> 임의 문자의 최대 반복
+#     # \. -> '.' 문자
+#     # .*?/ -> '/'이 나오기 전까지의 최소 반복
+#     p = re.compile(r'(.*\..*?)/')
+#     url_img_cover = re.search(p, url_img_cover).group(1)
+#
+#     result.append({
+#         'rank': rank,
+#         'title': title,
+#         'url_img_cover': url_img_cover,
+#         'artist': artist,
+#         'album': album,
+#     })
 
-# div_rank01 변수에 있는 문자열에서
-# <a href=....>(내용)</a>
-# 위의 (내용)그룹에 해당하는 부분을 title변수에 할당
-pattern_a_content = re.compile(r'<a.*?>(.*?)</a>')
-title = re.search(pattern_a_content, div_rank01).group(1)
-print(title)
+
+if __name__ == '__main__':
+    result = get_top100_list()
+    # for item in result:
+    #     print(item)
